197ca2ea65a3fb447edb2df15adacad428fcbc81
linh-tranha_03 <linh.tranhahcmut@hcmut.edu.vn>
2024-10-22 15:00:51 +0700

    Ex7

--- LAB/Core/Src/main.c
+++ LAB/Core/Src/main.c
@@ -91,89 +91,23 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int counter = 1;
-  while(1){
-   	 switch (counter){
- 		 case 1:
- 		 HAL_GPIO_TogglePin ( LED_1_GPIO_Port , LED_1_Pin );
- 		 break;
- 		 case 2:
- 		 HAL_GPIO_TogglePin ( LED_1_GPIO_Port , LED_1_Pin );
- 		 break;
- 		 case 3:
- 		 HAL_GPIO_TogglePin ( LED_2_GPIO_Port , LED_2_Pin );
- 		 break;
- 		 case 4:
- 		 HAL_GPIO_TogglePin ( LED_2_GPIO_Port , LED_2_Pin );
- 		 break;
- 		 case 5:
- 		 HAL_GPIO_TogglePin ( LED_3_GPIO_Port , LED_3_Pin );
- 		 break;
- 		 case 6:
- 		 HAL_GPIO_TogglePin ( LED_3_GPIO_Port , LED_3_Pin );
- 		 break;
- 		 case 7:
- 		 HAL_GPIO_TogglePin ( LED_4_GPIO_Port , LED_4_Pin );
- 		 break;
- 		 case 8:
- 		 HAL_GPIO_TogglePin ( LED_4_GPIO_Port , LED_4_Pin );
- 		 break;
- 		 case 9:
- 		 HAL_GPIO_TogglePin ( LED_5_GPIO_Port , LED_5_Pin );
- 		 break;
- 		 case 10:
- 		 HAL_GPIO_TogglePin ( LED_5_GPIO_Port , LED_5_Pin );
- 		 break;
- 		 case 11:
- 		 HAL_GPIO_TogglePin ( LED_6_GPIO_Port , LED_6_Pin );
- 		 break;
- 		 case 12:
- 		 HAL_GPIO_TogglePin ( LED_6_GPIO_Port , LED_6_Pin );
- 		 break;
- 		 case 13:
- 		 HAL_GPIO_TogglePin ( LED_7_GPIO_Port , LED_7_Pin );
- 		 break;
- 		 case 14:
- 		 HAL_GPIO_TogglePin ( LED_7_GPIO_Port , LED_7_Pin );
- 		 break;
- 		 case 15:
- 		 HAL_GPIO_TogglePin ( LED_8_GPIO_Port , LED_8_Pin );
- 		 break;
- 		 case 16:
- 		 HAL_GPIO_TogglePin ( LED_8_GPIO_Port , LED_8_Pin );
- 		 break;
- 		 case 17:
- 		 HAL_GPIO_TogglePin ( LED_9_GPIO_Port , LED_9_Pin );
- 		 break;
- 		 case 18:
- 		 HAL_GPIO_TogglePin ( LED_9_GPIO_Port , LED_9_Pin );
- 		 break;
- 		 case 19:
- 		 HAL_GPIO_TogglePin ( LED_10_GPIO_Port , LED_10_Pin );
- 		 break;
- 		 case 20:
- 		 HAL_GPIO_TogglePin ( LED_10_GPIO_Port , LED_10_Pin );
- 		 break;
- 		 case 21:
- 		 HAL_GPIO_TogglePin ( LED_11_GPIO_Port , LED_11_Pin );
- 		 break;
- 		 case 22:
- 		 HAL_GPIO_TogglePin ( LED_11_GPIO_Port , LED_11_Pin );
- 		 break;
- 		 case 23:
- 		 HAL_GPIO_TogglePin ( LED_12_GPIO_Port , LED_12_Pin );
- 		 break;
- 		 case 24:
- 		 HAL_GPIO_TogglePin ( LED_12_GPIO_Port , LED_12_Pin );
- 		 break;
-   	 }
-   	 counter++;
-   	 if (counter > 24){
-   		 counter = 1;
-   	 }
-   	 HAL_Delay(500);
-   	 }
-
+	  while (1){
+	  void clearAllClock(){
+	  	HAL_GPIO_WritePin ( LED_1_GPIO_Port , LED_1_Pin,RESET );
+	  	HAL_GPIO_WritePin ( LED_2_GPIO_Port , LED_2_Pin,RESET );
+	  	HAL_GPIO_WritePin	 ( LED_3_GPIO_Port , LED_3_Pin,RESET);
+	  	HAL_GPIO_WritePin ( LED_4_GPIO_Port , LED_4_Pin,RESET );
+	  	HAL_GPIO_WritePin ( LED_5_GPIO_Port , LED_5_Pin,RESET );
+	  	HAL_GPIO_WritePin	 ( LED_6_GPIO_Port , LED_6_Pin,RESET);
+	  	HAL_GPIO_WritePin ( LED_7_GPIO_Port , LED_7_Pin,RESET );
+	  	HAL_GPIO_WritePin	 ( LED_8_GPIO_Port , LED_8_Pin,RESET);
+	  	HAL_GPIO_WritePin ( LED_9_GPIO_Port , LED_9_Pin,RESET );
+	  	HAL_GPIO_WritePin ( LED_10_GPIO_Port , LED_10_Pin,RESET );
+	  	HAL_GPIO_WritePin	 ( LED_11_GPIO_Port , LED_11_Pin,RESET);
+	  	HAL_GPIO_WritePin ( LED_12_GPIO_Port , LED_12_Pin,RESET );
+	  }
+	  clearAllClock();
+	  }
 }
 
 /**
